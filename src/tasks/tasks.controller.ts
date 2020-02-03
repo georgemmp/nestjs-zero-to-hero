c400b264a74cd2f78ec570be281a4a6d4fca9dd9
georgemmp <machadogeorge.p@gmail.com>
2020-02-02 23:26:04 -0300

    Added taks model, service, controller and models

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+
+@Controller('tasks')
+export class TasksController {
+    constructor(private taskService: TasksService) { }
+
+    @Get()
+    getAll(): Task[] {
+        return this.taskService.getAll();
+    }
+
+    @Post()
+    createTask(@Body('title') title: string, @Body('description') description: string): Task {
+        return this.taskService.createTask(title, description);
+    }
+}
