fe42e05d87b12d583304a97148e07043275d89a6
georgemmp <machadogeorge.p@gmail.com>
2020-02-25 14:52:28 -0300

    Implemented authentication

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,13 @@
+import { IsString, MaxLength, MinLength, Matches } from 'class-validator';
+
+export class AuthCredentialDto {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    username: string;
+
+    @IsString()
+    @MinLength(6)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {message: 'Password too weak'})
+    password: string;
+}
---
+++ src/auth/user.repository.ts
@@ -0,0 +1,15 @@
+import { Repository, EntityRepository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthCredentialDto } from './dto/auth-credentials.dto';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+    async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
+        const {username, password} = authCredentialDto;
+
+        const user = new User();
+        user.username = username;
+        user.password = password;
+        user.save();
+    }
+}
