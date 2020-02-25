29cea198bd308807925a353463abbb3186523709
georgemmp <machadogeorge.p@gmail.com>
2020-02-25 15:32:22 -0300

    Added security on password store

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,8 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialDto } from './dto/auth-credentials.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -9,7 +11,21 @@ export class UserRepository extends Repository<User> {
 
         const user = new User();
         user.username = username;
-        user.password = password;
-        user.save();
+        user.salt = await bcrypt.genSalt();
+        user.password = await this.hashPassword(password, user.salt);
+
+        try {
+            await user.save();
+        } catch (error) {
+            if (error.code === 'ER_DUP_ENTRY') {
+                throw new ConflictException('Username already exists');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
+    }
+
+    private async hashPassword(password: string, salt: string): Promise<string> {
+        return bcrypt.hash(password, salt);
     }
 }
